96f2037df68eb49e24fbf88042eedd5fbf6f3189
Obuleswar <obuleswar88@gmail.com>
2025-08-27 09:13:50 +0000

    Add this <script type="module">   // Import the functions you need from

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBAOO7Gn1Y64dx_imZK0v_q_HaRzc6UYqc",
-  authDomain: "earning-app-ff02b.firebaseapp.com",
-  projectId: "earning-app-ff02b",
-  storageBucket: "earning-app-ff02b.firebasestorage.app",
-  messagingSenderId: "195041352341",
-  appId: "1:195041352341:web:517d304706a851dcc7999c",
-  measurementId: "G-FFVVEGQRFH"
+  apiKey: "AIzaSyB-nVUvhTlHKFvbuoDL0yQPTHqDcbCkv7k",
+  authDomain: "mining-6efe8.firebaseapp.com",
+  projectId: "mining-6efe8",
+  storageBucket: "mining-6efe8.firebasestorage.app",
+  messagingSenderId: "1028644513915",
+  appId: "1:1028644513915:web:8e3304deae9222d3005ade",
+  measurementId: "G-QTQL8SHGWQ"
 };
 
 // Initialize Firebase
