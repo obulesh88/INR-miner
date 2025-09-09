8a60964c8c06796f5f10e50fca9ecef8d4dd8731
Obuleswar <obuleswar88@gmail.com>
2025-09-09 07:42:28 +0000

    module.exports = {   async headers() {     return [       {         sour

--- next.config.ts
+++ next.config.ts
@@ -24,6 +24,17 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'X-Frame-Options', value: 'ALLOWALL' },
+          { key: 'Content-Security-Policy', value: "frame-ancestors *" },
+        ],
+      },
+    ]
+  },
 };
 
 export default nextConfig;
