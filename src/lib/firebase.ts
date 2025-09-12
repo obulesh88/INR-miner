4ee80da2f05eea878c5d2e094591025cd1bbcd5e
Obuleswar <obuleswar88@gmail.com>
2025-09-12 01:04:17 +0000

    import { initializeFirestore, enableIndexedDbPersistence } from "firebas

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,7 @@
 
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, enableIndexedDbPersistence, initializeFirestore } from "firebase/firestore";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -17,6 +17,21 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
-const db = getFirestore(app);
+
+// Initialize Firestore with offline persistence
+let db;
+if (typeof window !== 'undefined') {
+  db = initializeFirestore(app, {});
+  enableIndexedDbPersistence(db).catch((err) => {
+    if (err.code == 'failed-precondition') {
+      console.log("Persistence failed: Multiple tabs open");
+    } else if (err.code == 'unimplemented') {
+      console.log("Persistence is not supported in this browser");
+    }
+  });
+} else {
+    db = getFirestore(app);
+}
+
 
 export { app, auth, db };
