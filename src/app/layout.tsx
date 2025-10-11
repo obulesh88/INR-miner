74d898075555c11e2345e4fbf9208e7245ef2f10
Obuleswar <obuleswar88@gmail.com>
2025-10-11 15:38:30 +0000

    VERCEL_PREVIEW_FEEDBACK_ENABLED=0

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,6 @@ export default function RootLayout({
         
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://www.inrminer.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
         <meta name="theme-color" content="#1976d2" />
         
       </head>
